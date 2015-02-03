d2d9947ca52e834cf48556cd51c4ca871e7b859c
mshaf <michael.shafland@gmail.com>
2015-02-02 22:22:07 -0500

    added fadeOut

--- Coupons/main.js
+++ Coupons/main.js
@@ -1,11 +1,12 @@
 var entitles = "This coupon entitles you to ";
 var clickOK = "Click OK to redeem this coupon";
 
-$("img").on("click", function(e) {
+$(function(){$("img").on("click", function(e) {
 	$("body").append('<div class="background"></div>');
     if (confirm(entitles + this.title.toLowerCase() + "\n" + clickOK)){
- 		$(this).addClass("faded");
+ 		$(this).fadeOut("slow");
  	}
  	$(".background").remove();
 
  });
+});
